50e2034bb3e56d18a23dfba98eb8c4b1163c0f15
Ivan Candido <ivan.candido@powerhrg.com>
2021-06-03 17:24:36 -0400

    Completed Assignment

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const emptyH = document.querySelectorAll('.like-glyph')
+const fullH = document.querySelector('.activated')
+const emptyHArray = Array.prototype.slice.call(emptyH)
+const modal = document.querySelector('#modal')
 
-// Your JavaScript code goes here!
+const init = () => {
+  emptyHArray.forEach(element => {
+    element.addEventListener("click", () =>{
+      mimicServerCall()
+      .then(object => {
+        if (element.innerHTML === EMPTY_HEART){
+          element.classList = 'activated-heart'
+          element.innerHTML = FULL_HEART
+          emptyHArray.push(element)
+        } else {
+          element.classList = 'like-glyph'
+          element.innerHTML = EMPTY_HEART
+          emptyHArray.push(element)
+        }
+      })
+      .catch(error => {
+        const modal = document.querySelector('#modal')
+        modal.classList.remove('hidden')
+        setTimeout(() =>{
+          modal.classList = 'hidden'
+        }, 3000)
+      })
+    })
+  });
+}
 
 
 
+document.addEventListener("DOMContentLoaded", init)
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
